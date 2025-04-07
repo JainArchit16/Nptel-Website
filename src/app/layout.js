1ac2feae10a48d147fa5a21421c7f08ed46fba1f
Daksh-134 <117268999+Daksh-134@users.noreply.github.com>
2025-04-07 16:04:58 +0530

    Fixed logo - FINAL COMMIT

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,6 @@ import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "@/components/AuthProvider";
 import { motion } from "framer-motion";
-import Head from "next/head";
-// import { SessionProvider } from "next-auth/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,33 +19,27 @@ export const metadata = {
   title: "Home",
   description: "One stop solution for NPTEL exams",
   icons: {
-    icon: "/mainLogo.jpg",
+    icon: "/favicon.ico",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <AuthProvider>
-        <html lang="en" className="h-full">
-          <Head>
-            <link rel="icon" href="/mainLogo.jpg" />
-          </Head>
-          <body className="h-full w-full">
-            <motion
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 1.3 }}
-            >
-              <Toaster />
-              <Navbar />
-              {children}
-            </motion>
-            <div id="modal-root"></div>
-          </body>
-        </html>
-      </AuthProvider>
-    </>
+    <html lang="en" className="h-full">
+      <body className="h-full w-full">
+        <AuthProvider>
+          <motion
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Navbar />
+            {children}
+            <Toaster />
+          </motion>
+        </AuthProvider>
+      </body>
+    </html>
   );
 }
